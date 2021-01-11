675ec3adda823cf37ca6c61cb7820dbc7514d7c3
medici305 <asvpescobar@hotmail.com>
2021-01-11 15:21:58 +0000

    Section 1 complete

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../../node_modules/hover.css/css/hover.css';
+
+const Header = () => {
+    return (
+        <section className="header">
+            <div className="nav-bar">
+                <h1>loopstudios</h1>
+                <nav>
+                    <li className="hvr-underline-from-center">About</li>
+                    <li className="hvr-underline-from-center">Careers</li>
+                    <li className="hvr-underline-from-center">Events</li>
+                    <li className="hvr-underline-from-center">Products</li>
+                    <li className="hvr-underline-from-center">Support</li>
+                </nav>
+            </div>
+            <div className="banner">
+                <div className="box">
+                    <h3>IMMERSIVE EXPERIENCES THAT DELIVER</h3>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Header;
